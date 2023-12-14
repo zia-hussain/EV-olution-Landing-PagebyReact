6839766c76a36dd00dff7081c8473a5c16f8e663
zia-hussain <syedziashahgillani76@gmail.com>
2023-12-14 20:12:11 +0500

    navbar component

--- ev-website/src/Components/Background/Navbar/Hero/Navbar.jsx
+++ ev-website/src/Components/Background/Navbar/Hero/Navbar.jsx
@@ -1,17 +1,32 @@
-import './Navbar.css'
+import { useState } from "react";
+import "./Navbar.css";
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className='nav'>
-
-        <div className="nav-logo">Ev-olution</div>
-        <ul className='nav-menu'>
-            <li>Home</li>
-            <li>Explore</li>
-            <li>About</li>
-            <li className='nav-contact'>Contact</li>
-        </ul>
+    <div className="nav">
+      <div className="nav-logo">Ev-olution</div>
+      <ul className="nav-menu" style={{ display: open ? "flex" : "none" }}>
+        <li>Home</li>
+        <li>Explore</li>
+        <li>About</li>
+        <li className="nav-contact">Contact</li>
+      </ul>
+      {!open ? (
+        <i
+          id="open"
+          className="fa-solid fa-bars"
+          onClick={() => setOpen(true)}
+        ></i>
+      ) : (
+        <i
+          id="close"
+          className="fa-solid fa-xmark"
+          onClick={() => setOpen(false)}
+        ></i>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
