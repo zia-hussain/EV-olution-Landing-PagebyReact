93c08181496a2afe2284fad73f2a59d4585345ac
zia-hussain <syedziashahgillani76@gmail.com>
2023-12-14 20:11:50 +0500

    background

--- ev-website/src/Components/Background/Background.jsx
+++ ev-website/src/Components/Background/Background.jsx
@@ -8,16 +8,16 @@ import image3 from '../../Assets/image3.png';
 const Background = ({ playStatus, heroCount }) => {
   if (playStatus) {
     return (
-      <video className='background' autoPlay loop muted>
+      <video className='background fade-in' autoPlay loop muted>
         <source src={video1} type='video/mp4' />
       </video>
     )
   } else if (heroCount === 0) {
-    return <img src={image1} className='background' alt="" />;
+    return <img src={image1} className='background fade-in' alt="" />;
   } else if (heroCount === 1) {
-    return <img src={image2} className='background' alt="" />;
+    return <img src={image2} className='background fade-in' alt="" />;
   } else if (heroCount === 2) {
-    return <img src={image3} className='background' alt="" />;
+    return <img src={image3} className='background fade-in' alt="" />;
   }
 };
 
